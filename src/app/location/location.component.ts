55fc89ba1b5082d76889170f69607841d1f9aaff
sebabuonincontro <bruno.sebastian.buonincontro@monsanto.com>
2017-08-26 10:09:04 -0300

    added polling forecasts from locations

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -3,7 +3,9 @@ import {ActivatedRoute} from "@angular/router";
 import {BoardService} from "../services/board.service";
 import {Location} from "../domain/location";
 import {LocationService} from "../services/location.service";
-
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
+import {Board} from "../domain/board";
 
 @Component({
   templateUrl: "./location.component.html",
@@ -23,21 +25,29 @@ export class LocationComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe( params => {
       this.boardName = params['boardName'].toString();
-      this.getLocations();
-    })
+
+      this.getLocations().subscribe( board => this.getForecasts(board));
+    });
+
+    Observable.interval(120000) //2 minutes
+      .switchMap( () => this.getLocations())
+      .subscribe( board => this.getForecasts(board));
   }
 
-  getLocations() {
-    return this.boardService.getLocationsBy(this.boardName).subscribe( board => {
-      this.locations = board.locations;
-      this.locations.forEach( location => {
-        this.locationService.getWithNewsBy(location.woeid).subscribe( forecast => {
-          location.news = forecast.news;
-          location.forecasts = forecast.forecasts;
-        })
-      });
-    })
+  getLocations(): Observable<Board> {
+    return this.boardService.getLocationsBy(this.boardName)
   }
+
+  getForecasts(board: Board){
+    this.locations = board.locations;
+    this.locations.forEach( location => {
+      this.locationService.getWithNewsBy(location.woeid).subscribe( forecast => {
+        location.news = forecast.news;
+        location.forecasts = forecast.forecasts;
+      })
+    });
+  }
+
 
   save() {
     this.locationService.save(this.boardName, this.locationName).subscribe( newLocation => {
